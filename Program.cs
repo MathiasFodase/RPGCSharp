08aa32393ced24f8b48454c4923be8442713280d
MathiasFodase <matheus.hldomingues@gmail.com>
2023-04-30 01:46:27 -0300

    Arquivos que nao subiram no ultimo commit

--- Program.cs
+++ Program.cs
@@ -8,3 +8,7 @@ Console.WriteLine("What is your Player Name?     ");
 String playername = Console.ReadLine();
 
 Console.WriteLine($"Welcome to the game {playername}!");
+
+//Para adicionar a modificacao o comando e git add .
+//Para comitar o programa o comando e git commit -m "nome do seu codigo"
+//Para sincronizar com a nuvem voce usa o comando git push
